c4dcf09b607cdfd0e0ca0a365e014064f9200930
Samuel Etver <samuel.etver@gmail.com>
2022-12-11 22:32:34 +0500

    update feeder script

--- Challenge3/Assets/Scripts/Feeder.cs
+++ Challenge3/Assets/Scripts/Feeder.cs
@@ -7,13 +7,11 @@ public class Feeder : MonoBehaviour
 {
     public List<GameObject> FruitsList;
 
-    // Start is called before the first frame update
     void Start()
     {
         StartCoroutine(Feed());
     }
 
-    // Update is called once per frame
     void Update()
     {
 
@@ -28,7 +26,7 @@ public class Feeder : MonoBehaviour
             yield return new WaitForSeconds(0.3f);
             var index = Random.Range(0, n);
             var fruitPrefab = FruitsList[index];
-            Vector3 fruitPosition = transform.TransformPoint(Vector3.zero);
+            Vector2 fruitPosition = transform.TransformPoint(new Vector2(0, -1.9f));
             var fruit = Instantiate(fruitPrefab, fruitPosition, Quaternion.Euler(0f, 0f, Random.Range(0f, 360f)));
             var fruitScale = 0.08f;
             fruit.transform.localScale -= new Vector3(fruitScale, fruitScale, fruitScale);

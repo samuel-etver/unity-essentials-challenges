4f8a95620355a16d879bb9cee36e738a44f431b3
Samuel Etver <samuel.etver@gmail.com>
2022-12-18 00:02:12 +0500

    challenge 2. create plank 2 rotation script

---
+++ Challenge2/Assets/Scripts/Plank1Movement.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Transactions;
+using UnityEngine;
+
+public class Plank1Movement : MonoBehaviour
+{
+    void Start()
+    {
+        Debug.Log("Started Plank 1 Movement");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Challenge2/Assets/Scripts/Plank2Rotation.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Plank2Rotation : MonoBehaviour
+{
+    public float SpeedFactor = 50.0f;
+
+    private void Start()
+    {
+        Debug.Log("Started Plank 2 Rotation");
+    }
+
+
+    private void Update()
+    {
+        transform.Rotate(Vector3.up * Time.deltaTime * SpeedFactor);
+    }
+}
